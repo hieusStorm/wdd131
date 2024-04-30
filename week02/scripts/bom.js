d7a92feb3ba50eb75ffffc39a0bf2b8212b029b9
hieusStorm <hyrum555@gmail.com>
2024-04-29 17:41:12 -0700

    Added functionality to the BoM project

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,11 +1,28 @@
 const favchap = document.getElementById("favchap");
 const button = document.querySelector("button");
 const htmlList = document.getElementById("list");
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
 
-li.textContent = favchap.value;
-deleteButton.textContent = "X";
+button.addEventListener('click', () => {
+    if (favchap.value.trim() != "") {
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
 
-li.appendChild(deleteButton);
-htmlList.appendChild(li);
+        li.textContent = favchap.value;
+        deleteButton.textContent = "X";
+
+        li.appendChild(deleteButton);
+        htmlList.appendChild(li);
+
+        deleteButton.addEventListener('click', () => {
+
+            htmlList.removeChild(li);
+            favchap.focus();
+        
+        });
+
+    }
+
+    favchap.value = "";
+    favchap.focus();
+
+});
