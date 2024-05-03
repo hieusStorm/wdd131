4128a11c7dc228e9e19fc959d20d1d97892a7f36
hieusStorm <hyrum555@gmail.com>
2024-05-03 16:36:43 -0700

    Added the ability to toggle nav menu in mobile

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,4 @@
+//Place the footer information
 const currrentDate = new Date()
 const yearTag = document.querySelector("#currentyear")
 const lastModifiedTag = document.querySelector("#lastModified")
@@ -5,3 +6,12 @@ const lastModified = document.lastModified
 
 yearTag.innerHTML = currrentDate.getFullYear()
 lastModifiedTag.innerHTML = `Last Modification: ${lastModified}`
+
+
+//Toggle navigation menu in mobile
+const menuButton = document.getElementById("menu");
+const nav = document.querySelector("nav");
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('open');
+    nav.classList.toggle('open');
+});
