ee94a064d98670b9ac67c9e50eb830af830b4a1a
hieusStorm <hyrum555@gmail.com>
2024-05-30 09:34:07 -0700

    got the navigation set up

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -1,3 +1,4 @@
+// add footer info
 const currrentDate = new Date();
 const yearTag = document.querySelector("#currentyear");
 const lastModifiedTag = document.querySelector("#lastModified");
@@ -5,3 +6,16 @@ const lastModified = document.lastModified;
 
 yearTag.innerHTML = currrentDate.getFullYear();
 lastModifiedTag.innerHTML = `Last Modification: ${lastModified}`;
+
+//add nav toggle
+const navButton = document.getElementById("menu");
+const nav = document.querySelector("nav");
+navButton.addEventListener("click", ()=> {
+    if(navButton.textContent == "≡") {
+        navButton.textContent = "X";
+    } else {
+        navButton.textContent = "≡";
+    }
+    navButton.classList.toggle("open");
+    nav.classList.toggle("open");
+});
